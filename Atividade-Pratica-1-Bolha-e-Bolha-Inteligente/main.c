fe22276cd005129a61f584143114a37f2a2ecd88
flavio055063 <77984647+flavio055063@users.noreply.github.com>
2023-03-13 15:05:23 -0300

    Add files via upload

---
+++ Atividade-Pratica-1-Bolha-e-Bolha-Inteligente/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bolha.h"
+#include <time.h>
+
+int main(void){
+  char nomeArquivo[4] = "nome";
+  int qtNumeros = 500;
+  int tam = 500;
+  int vet[500];
+
+  geraNumerosAleatorios(qtNumeros, nomeArquivo);
+  carregaVetor(vet, tam, nomeArquivo);
+  imprimeVetor(vet, tam);
+  bolhaInteligente(vet, tam);
+  imprimeVetor(vet, tam);
+
+
+}
